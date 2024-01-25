b822a4f9706c0aae3d6db7ebb248e7bfaffce03f
Your Name <you@example.com>
2024-01-25 22:17:56 +0800

    fd

---
+++ app.js
@@ -0,0 +1,11 @@
+const Menu_Nav = document.querySelector(".Menu_Nav")
+const NavLinks = document.querySelector("nav")
+
+
+
+
+
+//FUNCTION FOR NAV-MENU
+Menu_Nav.addEventListener("click",()=>{
+     NavLinks.classList.toggle("activeNav")
+})
